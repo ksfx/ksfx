c1a7aae56df9616c369bfbaa4f1162cfedce1f5f
Kejo Starosta <kstarosta@puck257.startdedicated.net>
2024-08-15 10:44:53 +0200

    KSFX default start

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-java -Xmx5G -Xms5G -Dserver.port=8888 -jar target/KSFX-0.0.1-SNAPSHOT.jar
+java -Xmx30G -Xms30G -jar target/KSFX-0.0.1-SNAPSHOT.jar
+# java -Xmx5G -Xms5G -Dserver.port=8888 -jar target/KSFX-0.0.1-SNAPSHOT.jar
